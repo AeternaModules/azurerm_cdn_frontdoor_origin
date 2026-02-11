a59c6c84770a610c83f14e75c42aff5dbe865fcf
Daniel Allen <dan@aeternagroup.net>
2026-02-11 23:47:59 +0000

    Update to schema version v4.59.0

--- variables.tf
+++ variables.tf
@@ -25,16 +25,16 @@ EOT
     certificate_name_check_enabled = bool
     host_name                      = string
     name                           = string
-    enabled                        = optional(bool, true)
-    http_port                      = optional(number, 80)
-    https_port                     = optional(number, 443)
+    enabled                        = optional(bool)   # Default: true
+    http_port                      = optional(number) # Default: 80
+    https_port                     = optional(number) # Default: 443
     origin_host_header             = optional(string)
-    priority                       = optional(number, 1)
-    weight                         = optional(number, 500)
+    priority                       = optional(number) # Default: 1
+    weight                         = optional(number) # Default: 500
     private_link = optional(object({
       location               = string
       private_link_target_id = string
-      request_message        = optional(string, "Access request for CDN FrontDoor Private Link Origin")
+      request_message        = optional(string) # Default: "Access request for CDN FrontDoor Private Link Origin"
       target_type            = optional(string)
     }))
   }))
